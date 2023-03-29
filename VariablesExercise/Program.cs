596a8a3f7ee83a02283a8786b2b9198a7d912fa1
Autif Kamal <kautif@gmail.com>
2023-03-28 19:19:51 -0500

    added dog story with interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Lassie";
+            int legs = 8;
+            char letter = 'L';
+            bool pottyTrained = false;
+            double runSpeed = 98.163;
+            decimal sleepHours = 9.54m;
+
+            Console.WriteLine($"Meet my dog, {dogName}. It has {legs} legs and a capital {letter} on its collar and dog bowl. " +
+                $"Unfortunately, it is {pottyTrained} that it is potty trained. But, it runs at speeds of {runSpeed} miles per hour when it has to go. It sleeps for around {sleepHours} hours per day");
         }
     }
 }
